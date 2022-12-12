78c5ab6df91857c5d4457bb96f1d2fd41276867c
Eren <eren.alparslan@appcent.mobi>
2022-12-12 20:55:21 +0300

    -[UPDATES] ChartPointDataset

--- chartcore/src/main/java/com/github/chartcore/data/dataset/ChartPointDataset.kt
+++ chartcore/src/main/java/com/github/chartcore/data/dataset/ChartPointDataset.kt
@@ -1,5 +1,7 @@
 package com.github.chartcore.data.dataset
 
+import com.github.chartcore.common.BorderAlign
+import com.github.chartcore.common.BorderJoinStyle
 import com.github.chartcore.common.ChartTypes
 import com.github.chartcore.common.Dataset
 
@@ -11,9 +13,51 @@ open class ChartPointDataset(
     private var label: String? = null,
     private var data: List<Point> = emptyList(),
     private var backgroundColor: String? = null,
+    private var borderAlign: String? = null,
+    private var borderColor: String? = null,
+    private var borderJoinStyle: String? = null,
+    private var borderRadius: Int? = null,
+    private var borderWidth: Int? = null,
+    private var borderSkipped: Boolean? = null,
+    private var circumference: Int? = null,
+    private var clip: Int? = null,
+    private var hoverBackgroundColor: String? = null,
+    private var hoverBorderColor: String? = null,
+    private var hoverBorderJoinStyle: String? = null,
+    private var hoverBorderWidth: Int? = null,
+    private var hoverOffset: Int? = null,
+    private var offset: Int? = null,
+    private var rotation: Int? = null,
+    private var spacing: Int? = null,
+    private var weight: Int? = null,
+    private var barPercentage: Int? = null,
+    private var barThickness: Int? = null,
+    private var fill: Boolean? = null,
+    private var tension: Int? = null
 ): Dataset {
     fun type(type: ChartTypes) = apply { this.type = type.type }
     fun label(label: String) = apply { this.label = label }
     fun data(data: List<Point>) = apply { this.data = data }
     fun backgroundColor(backgroundColor: String) = apply { this.backgroundColor = backgroundColor }
+    fun borderAlign(align: BorderAlign) = apply { this.borderAlign = align.align }
+    fun borderColor(color: String?) = apply { this.borderColor = color }
+    fun borderJoinStyle(style: BorderJoinStyle) = apply { this.borderJoinStyle = style.style }
+    fun borderRadius(radius: Int) = apply { this.borderRadius = radius }
+    fun borderWidth(width: Int) = apply { this.borderWidth = width }
+    fun circumference(circumference: Int) = apply { this.circumference = circumference }
+    fun clip(clip: Int) = apply { this.clip = clip }
+    fun hoverBackgroundColor(color: String) = apply { this.hoverBackgroundColor = color }
+    fun hoverBorderColor(color: String) = apply { this.hoverBorderColor = color }
+    fun hoverBorderJoinStyle(style: BorderJoinStyle) = apply { this.hoverBorderJoinStyle = style.style }
+    fun hoverBorderWidth(width: Int) = apply { this.hoverBorderWidth = width }
+    fun hoverOffset(offset: Int) = apply { this.hoverOffset = offset }
+    fun offset(offset: Int) = apply { this.offset = offset }
+    fun rotation(rotation: Int) = apply { this.rotation = rotation }
+    fun spacing(spacing: Int) = apply { this.spacing = spacing }
+    fun weight(weight: Int) = apply { this.weight = weight }
+    fun borderSkipped(borderSkipped: Boolean) = apply { this.borderSkipped = borderSkipped }
+    fun barPercentage(percentage: Int?) = apply { this.barPercentage = percentage }
+    fun barThickness(thickness: Int?) = apply { this.barThickness = thickness }
+    fun fill(fill: Boolean?) = apply { this.fill = fill }
+    fun tension(tension: Int?) = apply { this.tension = tension }
 }
