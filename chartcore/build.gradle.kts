4feb7be8197ffe610fb5175209752c22824a388b
Eren <eren.alparslan@appcent.mobi>
2022-12-15 16:16:21 +0300

    -[ADD] github actions

--- chartcore/build.gradle.kts
+++ chartcore/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
@@ -36,7 +37,64 @@ android {
 dependencies {
     implementation(Deps.gson)
     implementation(Deps.core)
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+}
+
+afterEvaluate {
+    configure<PublishingExtension> {
+        val artifact = "charts"
+        val publishedGroupId = "com.github.chartcore"
+        val libraryName = "ChartCore-Kotlin"
+
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = publishedGroupId
+                artifactId = artifact
+                version = "1.0.0"
+
+                artifact(tasks.getByName("sourcesJar"))
+                artifact("$buildDir/outputs/aar/${artifactId}-release.aar") {
+                    builtBy(tasks.getByName("assemble"))
+                }
+
+                pom {
+                    packaging = "aar"
+                    name.set(libraryName)
+                    licenses {
+                        license {
+                            name.set("The Apache Software License, Version 2.0")
+                            url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                        }
+                    }
+                    withXml {
+                        val dependenciesNode = asNode().appendNode("dependencies")
+                        // note: replace with the desired configuration (ex: api, testImplementation, etc...)
+                        configurations.getByName("implementation") {
+                            dependencies.forEach {
+                                val dependencyNode = dependenciesNode.appendNode("dependency")
+                                dependencyNode.appendNode("groupId", it.group)
+                                dependencyNode.appendNode("artifactId", it.name)
+                                dependencyNode.appendNode("version", it.version)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        repositories {
+            maven {
+                name = "GitHubPackages"
+                url = uri("https://maven.pkg.github.com/ErenAlpaslan/ChartCore")
+                credentials {
+                    username = System.getenv("GPR_USER")
+                    password = System.getenv("GPR_KEY")
+                }
+            }
+        }
+    }
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    archiveClassifier.set("sources")
+    from(android.sourceSets.getByName("main").java.srcDirs)
 }
