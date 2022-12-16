159184711ff0315e6fce4a7c6c707030e5457a46
Eren <eren.alparslan@appcent.mobi>
2022-12-16 23:10:52 +0300

    -Minor change

--- chartcore/build.gradle.kts
+++ chartcore/build.gradle.kts
@@ -44,7 +44,6 @@ afterEvaluate {
     configure<PublishingExtension> {
         val artifact = "chartcore"
         val publishedGroupId = "com.github.charts"
-        val libraryName = "ChartCore-Kotlin"
 
         publications {
             create<MavenPublication>("maven") {
@@ -53,40 +52,6 @@ afterEvaluate {
                 version = "1.0.0"
 
                 artifact(tasks.getByName("sourcesJar"))
-
-                pom {
-                    packaging = "aar"
-                    name.set(libraryName)
-                    description.set("ChartCore-Kotlin library")
-                    developers {
-                        developer {
-                            id.set("ErenAlpaslan")
-                            name.set("Eren Alpaslan")
-                            email.set("erenalpaslan@gmail.com")
-                            url.set("https://github.com/ErenAlpaslan")
-                        }
-                    }
-                    scm {
-                        connection.set("scm:git:github.com/ErenAlpaslan/ChartCore.git")
-                        developerConnection.set("scm:git:ssh://github.com/ErenAlpaslan/ChartCore.git")
-                        url.set("https://github.com/ErenAlpaslan/ChartCore/tree/master")
-                    }
-                    withXml {
-                        val dependenciesNode = asNode().appendNode("dependencies")
-                        configurations.getByName("implementation") {
-                            dependencies.forEach {
-                                val dependencyNode = dependenciesNode.appendNode("dependency")
-                                dependencyNode.appendNode("groupId", it.group)
-                                dependencyNode.appendNode("artifactId", it.name)
-                                dependencyNode.appendNode("version", it.version)
-                            }
-                        }
-                    }
-                    distributionManagement {
-                        downloadUrl.set("https://maven.pkg.github.com/ErenAlpaslan/ChartCore")
-                    }
-
-                }
             }
         }
 
